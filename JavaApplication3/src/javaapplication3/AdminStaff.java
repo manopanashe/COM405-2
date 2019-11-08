3e5b851cebd2b76776f591e66e3b577358ac0460
Panashe Mano <4manop68@solent.ac.uk>
2019-11-08 11:56:16 +0000

    third commit

--- JavaApplication3/src/javaapplication3/AdminStaff.java
+++ JavaApplication3/src/javaapplication3/AdminStaff.java
@@ -17,4 +17,26 @@ public class AdminStaff extends Employee
      super(nameln, Payln, NumofHoursln, HolidayEntiltementln);   
 
     }
+
+    public Manager getAssignedManager() {
+        return assignedManager;
+    }
+
+    public void setAssignedManager(Manager assignedManager) {
+        this.assignedManager = assignedManager;
+    }
+    
+    public String getDeatils()
+    {   
+        
+        String ret = super.getDetails();
+        if(assignedManager != null){
+        ret += assignedManager.getName();
+        }
+        else{
+        ret += "No manager assigned";
+        }
+        return ret;
+}
+     
 }
--- JavaApplication3/src/javaapplication3/JavaApplication3.java
+++ JavaApplication3/src/javaapplication3/JavaApplication3.java
@@ -18,9 +18,14 @@ public class JavaApplication3 {
         // TODO code application logic here
        
         Employee e1 = new Employee("Panashe", 10, 5, 6);
+        Manager m1 = new Manager("mano",20, 10,10, 8);
+        AdminStaff a1 = new AdminStaff("rachel", 10, 10, 11);
         
-        e1.setName("panashe");
-        e1.setNumofHours(10);
+        a1.setAssignedManager(m1);
+        
+         System.out.println(e1.getDetails());
+         System.out.println(m1.getDetails());
+       System.out.println(a1.getDetails());
         
     
     }
